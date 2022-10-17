00f9094f8151a037634059e65c2d15e8fe901f00
xvart <xvart@languid.me.uk>
2022-10-17 12:13:20 +1100

    link and unlink added manager stuff in case I ever need it

--- mptr.c
+++ mptr.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include "mptr.h"
 
+
+/**
+ * Find an empty MPTRENTRY
+ */
+MPTRENTRY *managed_pointer_findEmpty(struct MANAGEDPTR_HEAD *head) {
+	MPTRENTRY *np;
+	LIST_FOREACH(np, head, entries)
+		if( np->used == 0 ) {
+			return np;
+		}
+	return NULL;
+}
+
+/**
+ * Allocate a block of LIST entries.
+ */
+void managed_pointer_listAllocate(struct MANAGEDPTR_HEAD *head, int size) {
+	if( head->count == 0 ) {
+		for(int i =0; i < size; i++) {
+			LIST_INSERT_HEAD(head, (MPTRENTRY*) calloc(1, sizeof(MPTRENTRY)), entries);
+		}
+	}
+}
+
+/**
+ * Remove unused entries
+ */
+void managed_pointer_trim(struct MANAGEDPTR_HEAD *head, int size) {
+	MPTRENTRY *entry_ptr;
+	MPTRENTRY *last_ptr;
+	LIST_FOREACH(entry_ptr, head, entries)
+		if( entry_ptr->used == 0 ) {
+			last_ptr = *entry_ptr->entries.le_prev;
+			LIST_REMOVE(entry_ptr, entries);
+			free(entry_ptr);
+			entry_ptr = *last_ptr->entries.le_prev;
+		}
+}
+
 /**
  * free all the endtries in a list
  *
@@ -97,3 +136,50 @@ void managed_pointer_free(struct MANAGEDPTR_HEAD *head, void *vptr) {
 	return;
 }
 
+/**
+ * Show info on limits
+ */
+void managed_pointer_info(struct MANAGEDPTR_HEAD *head, FILE *fp) {
+	fprintf(fp, "Head: min %u, max %u, init %u, count %u\n", head->min, head->max, head->init, head->count);
+}
+
+/**
+ * Optional.
+ * Set the limits, why because people like that sort of thing.
+ * When max = 0 min/max allocation limits are ignored.
+ */
+void managed_pointer_setLimits(struct MANAGEDPTR_HEAD *head, int min, int max, int init) {
+	head->min = min;
+	head->max = max;
+	if( head->count = 0 )
+		managed_pointer_listAllocate(head, min);
+}
+
+/**
+ * Add a poiter to the list
+ */
+void *managed_pointer_link(struct MANAGEDPTR_HEAD *head, void *vptr, void (*handler)(void*)) {
+	MPTRENTRY *eptr = (MPTRENTRY*) calloc(1, sizeof(MPTRENTRY));
+	eptr->data = vptr
+	eptr->free_handler = handler;
+	LIST_INSERT_HEAD(head, eptr, entries);
+	return vptr;
+}
+
+/**
+ * Remove from list
+ */
+void *managed_pointer_unlink(struct MANAGEDPTR_HEAD *head, void *vptr) {
+	MPTRENTRY *np;
+	if( vptr != NULL ) {
+		LIST_FOREACH(np, head, entries)
+			if( np->data == vptr ) {
+				//~ fprintf(stderr, "mp_free: removing\n");
+				LIST_REMOVE(np, entries);
+				free(np);
+				return vptr;
+			}
+			fprintf(stderr, "mp_free: error attempting to remove unmanaged pointer\n");
+	}
+	return NULL;
+}
--- mptr.h
+++ mptr.h
@@ -5,38 +5,63 @@
 #include <stddef.h>
 #include <sys/queue.h>
 
+#define	LIST_HEADX(name, type)		\
+struct name {						\
+	/* Limits */					\
+	int min, max, init;				\
+	/* Number of entries */			\
+	int count; 						\
+	/* first element */				\
+	struct type *lh_first;			\
+}
+
+#define	LIST_HEAD_INITIALIZERX(head) { 0,0,0,0,NULL }
 
 typedef struct MPTRENTRY {
 	void *data;
-	void (*free_handler)(void*); /* if a complex free is needed */
-	LIST_ENTRY(MPTRENTRY) entries; /* Singly-linked List. */
+	/* Used when a complex free is needed */
+	void (*free_handler)(void*);
+	/* needed if I add a manager */
+	int used;
+	/* Doubly-linked List. */
+	LIST_ENTRY(MPTRENTRY) entries;
 } MPTRENTRY;
 
-LIST_HEAD(MANAGEDPTR_HEAD, MPTRENTRY);
-
+//~ LIST_HEAD(MANAGEDPTR_HEAD, MPTRENTRY);
+LIST_HEADX(MANAGEDPTR_HEAD, MPTRENTRY);
 
 /**
  * The ugly functions
  */
-void *managed_pointer_alloc(struct MANAGEDPTR_HEAD *, size_t , void (*)(void*));
-void *managed_pointer_calloc(struct MANAGEDPTR_HEAD *, size_t , size_t , void (*)(void*));
-void *managed_pointer_realloc(struct MANAGEDPTR_HEAD *, void *, size_t );
-void *managed_pointer_reallocarray(struct MANAGEDPTR_HEAD *, void *, size_t , size_t );
-void managed_pointer_free(struct MANAGEDPTR_HEAD *, void *);
+void *managed_pointer_link(struct MANAGEDPTR_HEAD *head, void *vptr, void (*handler)(void*));
+void *managed_pointer_unlink(struct MANAGEDPTR_HEAD *head, void *vptr);
 void managed_pointer_clear(struct MANAGEDPTR_HEAD *);
+void managed_pointer_free(struct MANAGEDPTR_HEAD *, void *);
+void managed_pointer_info(struct MANAGEDPTR_HEAD *, FILE *);
+void *managed_pointer_realloc(struct MANAGEDPTR_HEAD *, void *, size_t );
+void managed_pointer_setLimits(struct MANAGEDPTR_HEAD *, int , int , int);
+void *managed_pointer_alloc(struct MANAGEDPTR_HEAD *, size_t , void (*)(void*));
+void *managed_pointer_reallocarray(struct MANAGEDPTR_HEAD *, void *, size_t , size_t );
+void *managed_pointer_calloc(struct MANAGEDPTR_HEAD *, size_t , size_t , void (*)(void*));
 
 /**
  * Macro's with the magic
  */
 #define mp_init() \
-	struct MANAGEDPTR_HEAD managedptr_head = LIST_HEAD_INITIALIZER(managedptr_head)
+	struct MANAGEDPTR_HEAD managedptr_head = LIST_HEAD_INITIALIZERX(managedptr_head)
 
-#define mp_alloc(s, h) managed_pointer_alloc(&managedptr_head, s, h)
-#define mp_calloc(n, s, h) managed_pointer_calloc(&managedptr_head, n, s, h)
+#define mp_setLimits(min,max,init) managed_pointer_setLimits(&managedptr_head, min, max, init)
+#define mp_alloc(s) managed_pointer_alloc(&managedptr_head, s, NULL)
+#define mp_allocx(s, h) managed_pointer_alloc(&managedptr_head, s, h)
+#define mp_link(s, h) managed_pointer_alloc(&managedptr_head, s, h)
+#define mp_unlink(s) managed_pointer_alloc(&managedptr_head, s)
+#define mp_calloc(n, s) managed_pointer_calloc(&managedptr_head, n, s, NULL)
+#define mp_callocx(n, s, h) managed_pointer_calloc(&managedptr_head, n, s, h)
 #define mp_realloc(p, s) managed_pointer_realloc(&managedptr_head, p, s)
 #define mp_reallocarray(p, n, s) managed_pointer_reallocarray(&managedptr_head, p, n, s)
 #define mp_free(p) managed_pointer_free(&managedptr_head, p)
 #define mp_clear() managed_pointer_clear(&managedptr_head)
+#define mp_info(fp) managed_pointer_info(&managedptr_head, fp)
 
 #define mp_return(v) do { \
 	managed_pointer_clear(&managedptr_head); \
